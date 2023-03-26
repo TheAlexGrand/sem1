75cba3980e965b27cf91a3476a9aa4c5dd196bc9
AlexeySemkin <lehasemkin69@gmail.com>
2023-03-26 23:36:37 +0300

    create code for 4th hw

---
+++ dz1/hw4/Program.cs
@@ -0,0 +1,19 @@
+﻿/// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число:");
+string OneNum = Console.ReadLine();
+int One = Convert.ToInt32(OneNum);
+if (One <= 0)
+{
+    Console.WriteLine("Введите положительное число");
+}
+else
+{
+    for (int Count = 1;Count <= One; Count++)
+    {
+        if (Count % 2 == 0)
+        {
+            Console.Write($"{Count} ");
+        }
+    }
+}
