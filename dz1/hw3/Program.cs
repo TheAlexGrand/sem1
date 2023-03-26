fb3353bde3788804115ecb26c51b6d98c9280e6c
AlexeySemkin <lehasemkin69@gmail.com>
2023-03-26 23:20:57 +0300

    create code for 3th hw

---
+++ dz1/hw3/Program.cs
@@ -0,0 +1,12 @@
+﻿///Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число 1:");
+string OneNum = Console.ReadLine();
+int One = Convert.ToInt32(OneNum);
+
+if (One % 2 == 0){
+     Console.WriteLine($"Число {One} является четным");
+}
+else {
+    Console.WriteLine($"Число {One} является нечетным");
+}
