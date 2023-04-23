b91da27b41b326d05da66232d1dd1c095ca951df
AlexeySemkin <lehasemkin69@gmail.com>
2023-04-23 22:19:12 +0300

    create code for all hometask

---
+++ dz8/ts62/ts64/Program.cs
@@ -0,0 +1,25 @@
+﻿ /*
+ Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до M.
+ N = 5. -> "5, 4, 3, 2, 1"
+ N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
+ */
+ 
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+NaturalFrom1To2(number1, number2);
+
+
+void NaturalFrom1To2(int number1, int number2)
+{
+    if (number2 > number1)
+    {
+        return;
+    }
+    else
+    {
+        NaturalFrom1To2(number1, number2 + 1);
+        Console.Write(number2 + " ");
+    }
+}
