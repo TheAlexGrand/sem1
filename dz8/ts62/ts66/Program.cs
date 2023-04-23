c15b6297bcd617ecd44c42214f39fd5f833444bd
AlexeySemkin <lehasemkin69@gmail.com>
2023-04-23 22:07:09 +0300

    create code for all hometask

---
+++ dz8/ts62/ts66/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+ M = 1; N = 15 -> 120
+ M = 4; N = 8. -> 30
+ */
+ 
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(Sum(m - 1, n));
+}
+
+int Sum(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + Sum(m, n);
+        return result;
+    }
+}
